e96e9a1882b8de95e9794f6d42401256aa7c54fb
Brooke Jones <bjones782@gmail.com>
2019-06-23 12:49:43 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello, World!"
+    end 
+
+    get '/name' do
+       "My name is Brooke."
+    end 
+    
+    get '/hometown' do 
+        "My hometown is Douglasville."
+    end 
+
+    get '/favorite-song' do 
+        "My favorite song is Achy Breaky Heart."
+    end 
 end
