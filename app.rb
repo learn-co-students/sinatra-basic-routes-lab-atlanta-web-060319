920f6e5ef7ddf0bece90fe8f011e18d116d79ccb
Blake Stoner <stonerblake@gmail.com>
2019-06-23 22:09:21 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/name' do
+  "My name is Blake."
+end
+
+get '/hometown' do
+  "My hometown is College Park."
+end
+
+get '/favorite-song' do
+  "My favorite song is Ordinary People."
+end
+
 end
